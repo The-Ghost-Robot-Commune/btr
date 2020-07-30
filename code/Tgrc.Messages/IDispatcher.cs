7d89a25653b46bc5fcf1a45dd1cbd262d928531b
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2020-07-30 16:02:32 +0200

    added some initial interfaces inspired by the old bt project

---
+++ code/Tgrc.Messages/IDispatcher.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tgrc.Messages
+{
+	public interface IDispatcher
+	{
+
+		
+
+		/// <summary>
+		/// Sends all the messages that have arrived since the last dispatch
+		/// </summary>
+		void DispatchMessages();
+	}
+}
