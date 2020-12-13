debdb1a1a6769a94d134c0b5ddfc4fdbe77c03bf
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2020-12-13 16:47:32 +0100

    Create PayloadComponentIdComparer.cs

---
+++ code/Tgrc.Messages/PayloadComponentIdComparer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tgrc.Messages
+{
+	public class PayloadComponentIdComparer : IEqualityComparer<IPayloadComponentId>
+	{
+		public static readonly PayloadComponentIdComparer Instance = new PayloadComponentIdComparer();
+
+		private PayloadComponentIdComparer() { }
+
+		public bool Equals(IPayloadComponentId x, IPayloadComponentId y)
+		{
+			if (x == null && y == null)
+			{
+				return true;
+			}
+			if (x == null || y == null)
+			{
+				return false;
+			}
+			return x.Equals(y);
+		}
+
+		public int GetHashCode(IPayloadComponentId obj)
+		{
+			if (obj == null)
+			{
+				return 0;
+			}
+
+			return obj.GetHashCode();
+		}
+	}
+}
