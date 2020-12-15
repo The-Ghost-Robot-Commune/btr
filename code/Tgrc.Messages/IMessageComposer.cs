9302b025babe1a29cdde363c2e3ad0d2a12bc087
GrassVortex <58793188+GrassVortex@users.noreply.github.com>
2020-12-15 17:26:12 +0100

    Added some todo comments and some general reminders of design

--- code/Tgrc.Messages/IMessageComposer.cs
+++ code/Tgrc.Messages/IMessageComposer.cs
@@ -8,6 +8,8 @@ namespace Tgrc.Messages
 {
 	public interface IMessageComposer
 	{
+		// TODO Add tags to messages so they can easily be directed to only a subset of listeners. Useful for request/response patterns
+
 		IMessage Compose(params IPayloadComponent[] payloads);
 
 		IMessage Compose(IEnumerable<IPayloadComponent> payloads);
