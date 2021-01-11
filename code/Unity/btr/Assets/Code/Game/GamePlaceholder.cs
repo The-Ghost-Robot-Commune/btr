18fbaccb11165df0217bb7123ba713fdd8fd55cb
GrassVortex <grassvortex@gmail.com>
2021-01-11 05:16:02 +0100

    Added new btr Unity project with some code placeholders.

---
+++ code/Unity/btr/Assets/Code/Game/GamePlaceholder.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tgrc.btr
+{
+    public class GamePlaceholder : MonoBehaviour
+    {
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
