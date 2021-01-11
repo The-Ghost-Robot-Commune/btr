e2a44653787571d25675e4e424f5b20c287159ff
GrassVortex <grassvortex@gmail.com>
2021-01-11 21:10:22 +0100

    Added basic editor utility
    Added editor utility for saving all assets.
    Auto update of Unity files

---
+++ code/Unity/Tgrc.UnityEditorUtils/Assets/ManualSaveAssets.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEditor;
+using UnityEngine;
+
+namespace Tgrc.UnityEditorUtils.Assets
+{
+	class ManualSaveAssets
+	{
+		[MenuItem("AssetDatabase/Save all assets &s")]
+		private static void SaveAssets()
+		{
+			AssetDatabase.SaveAssets();
+			Debug.Log("Saved all assets.");
+		}
+	}
+}
