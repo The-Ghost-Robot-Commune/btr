6e5e512b2178db9cbbfdc3b9ecc5d0a8c2471bbf
GrassVortex <grassvortex@gmail.com>
2021-01-17 17:46:24 +0100

    Removed most references to ZeroFormatter from the code.

--- code/Tgrc.Messages.ConsoleTest/PayloadB.cs
+++ code/Tgrc.Messages.ConsoleTest/PayloadB.cs
@@ -3,16 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using ZeroFormatter;
+
 
 namespace Tgrc.Messages.ConsoleTest
 {
-	[ZeroFormattable]
 	public class PayloadB : IPayloadComponent
 	{
 		private static IPayloadComponentId id;
 
-		[IgnoreFormat]
 		public IPayloadComponentId Id { get { return id; } }
 
 		public static void SetId(IPayloadComponentId id)
--- code/Tgrc.Messages/ContextUtilities.cs
+++ code/Tgrc.Messages/ContextUtilities.cs
@@ -4,8 +4,6 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
-using ZeroFormatter;
-using static Tgrc.Messages.PayloadDefinition;
 
 namespace Tgrc.Messages
 {
